6dedeb0554dbc8fe9c9422dc647cb67360fba0d3
Omar Sandoval <omar@sandovoid.com>
2014-04-11 18:46:29 -0700

    Completed assignment 5

--- Homework5/Homework5.R
+++ Homework5/Homework5.R
@@ -44,3 +44,34 @@ summary(crab.mod.nb)
 # Confidence interval for the negative binomial model
 confint(crab.mod.nb, level = 0.95)
 
+#########################
+# R Code for Problem 3.18
+# (a)
+Attendance=c(404,286,443,169,222,150,321,189,258,223,211,215,
+             108,210,224,211,168,185,158,429,226,150,148)
+Arrests=c(308,197,184,149,132,126,110,101,99,81,79,78,
+          68,67,60,57,55,44,38,35,29,20,19)
+
+atten.arrest=data.frame(Attendance, Arrests)
+
+atten.fit2=glm(Arrests ~ Attendance, offset=log(Attendance), data=atten.arrest, family=poisson)
+summary(atten.fit2)
+
+atten.poi<-glm(Arrests ~ Attendance, offset=log(Attendance), data=atten.arrest, family=poisson())
+summary(atten.poi)
+predict.glm(atten.poi,type="terms")
+
+# (b)
+Attendance=c(404,286,443,169,222,150,321,189,258,223,211,215,
+             108,210,224,211,168,185,158,429,226,150,148)
+Arrests=c(308,197,184,149,132,126,110,101,99,81,79,78,
+          68,67,60,57,55,44,38,35,29,20,19)
+atten.arrest=data.frame(Attendance, Arrests)
+atten.fit2=glm(Arrests ~ Attendance, offset=log(Attendance), data=atten.arrest, family=poisson)
+summary(atten.fit2)
+predict.glm(atten.fit2,type="terms")
+
+# (c)
+
+plot(x = atten.arrest$Attendance, y = atten.arrest$Arrests, xlab = "Attendance", ylab = "Number of arrests",
+     main = "Attendance vs Arrest data set \n with poisson regression model fit", panel.first = grid(col = "gray", lty = "dotted"))
